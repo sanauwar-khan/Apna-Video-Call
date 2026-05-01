77f37439dc1177295bf85535b07a7db4cdf64d65
sanauwar khan <sanauwarkhan84@gmail.com>
2026-05-01 17:31:44 +0530

    fix: frotend add correctly

---
+++ frotend/src/App.js
@@ -0,0 +1,34 @@
+import "./App.css";
+import{ Route, BrowserRouter as Router, Routes} from "react-router-dom";
+import LandingPage from "./pages/landing";
+import { AuthProvider } from './contexts/AuthContext';
+import Authentication from "./pages/authentication";
+import VideoMeetComponent from "./pages/VideoMeet";
+import History from "./pages/history";
+import HomeComponent from "./pages/home";
+
+function App() {
+  return (
+    <>
+    <Router>
+
+< AuthProvider>
+
+    <Routes>
+
+<Route path="/" element={<LandingPage />}></Route>
+<Route path="/auth" element={<Authentication />}></Route>
+<Route path="/home" element={<HomeComponent />}></Route>
+<Route path="/history" element={<History />}></Route>
+<Route path="/:url" element={<VideoMeetComponent />}></Route>
+
+    </Routes>
+
+</AuthProvider>
+
+    </Router>
+    </>
+  );
+}
+
+export default App;
