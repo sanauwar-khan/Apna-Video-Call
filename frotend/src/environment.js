581b39277d1da85f6f9156d16c530fbfdec566f3
sanauwar khan <sanauwarkhan84@gmail.com>
2026-05-02 17:29:30 +0530

    deploy frontend

---
+++ frotend/src/environment.js
@@ -0,0 +1,10 @@
+
+let IS_PROD = true;
+const server = IS_PROD ?
+     
+     "https://apna-video-call-backend-pq0u.onrender.com" :
+     
+     "https://localhost:3000"
+
+
+export default server;
--- frotend/src/pages/VideoMeet.jsx
+++ frotend/src/pages/VideoMeet.jsx
@@ -13,11 +13,11 @@ import ScreenShareIcon from '@mui/icons-material/ScreenShare';
 import StopScreenShareIcon from '@mui/icons-material/StopScreenShare'
 import ChatIcon from '@mui/icons-material/Chat'
 import {useNavigate} from "react-router-dom"
-// import server from '../environment';
+import server from '../environment';
 
 
 
-const server_url = "http://localhost:3000";
+const server_url = server;
 
 var connections = {};
 
